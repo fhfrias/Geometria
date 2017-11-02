4bf455b3906627a7df4e4853e2060f56370aa831
Javi hf <friasjavi45@gmail.com>
2017-11-02 13:51:46 +0100

    Commit 3º 	modificado:    Geometria.java 	modificado:    TestGeometria.java
    Se han añadido dos métodos uno para calcular el area del circulo y otro para el area de un triangulo

--- TestGeometria.java
+++ TestGeometria.java
@@ -6,5 +6,13 @@ public class TestGeometria {
 
 	double areaRectangulo = Geometria.calcularAreaRectangulo(12.5,5.2);
 	System.out.println("El area del rectangulo es : " + areaRectangulo);
+	
+
+	double areaTriangulo = Geometria.calcularAreaTriangulo (12.3,3.6);
+	System.out.println("El area del triangulo es : " + areaTriangulo);
+	
+	double areaCirculo = Geometria.calcularAreaCirculo (2.5);
+	System.out.println("El area del circulo es : " + areaCirculo);
 	}
+
 }
