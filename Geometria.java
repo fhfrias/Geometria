6c030f827a293a2447043e4465882ac4eddabc74
Javi hf <friasjavi45@gmail.com>
2017-11-02 13:22:05 +0100

    nuevo archivo: .gitignore 	nuevo archivo: Geometria.java 	nuevo archivo: TestGeometria.java
    Geometria.java contiene dos métodos de geometria y Test trabaja la geometria

---
+++ Geometria.java
@@ -0,0 +1,10 @@
+public class Geometria {
+
+	public static double calcularAreaCuadrado (double lado){
+		return lado * lado;
+	}	
+	
+	public static double calcularAreaRectangulo (double base, double altura){
+	return base * altura;
+	} 
+}
---
+++ TestGeometria.java
@@ -0,0 +1,10 @@
+public class TestGeometria {
+	public static void main(String[] args){
+	
+	double areaCuadrado = Geometria.calcularAreaCuadrado(2.155);
+	System.out.println("Área del cuadrado es: " + areaCuadrado);
+
+	double areaRectangulo = Geometria.calcularAreaRectangulo(12.5,5.2);
+	System.out.println("El area del rectangulo es : " + areaRectangulo);
+	}
+}
